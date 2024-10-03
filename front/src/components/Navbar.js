e994f7ccb526f28cf9a949f374a6ebe9d3971b58
Renato <renatoft89@gmail.com>
2024-10-02 21:20:17 -0300

    feat: create navbar init

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,13 +1,24 @@
-import React, { memo } from 'react';
+'use client'
+
+import React from 'react'; 
+import { useRouter } from 'next/navigation'
+import Link from 'next/link';
 
 const Navbar = (() => {
+  const router = useRouter();
+  const currentPath = router.asPath;
   return (
-    <nav className="">
-      <div className="">
+    <nav className="container-md min-h-32 bg-sky-500 flex items-center justify-between ">
+      <div className="flex px-8">
+        {/* <Link to="/" className={currentPath === '/' ? 'disabled-link' : 'active-link'}>
+          Home
+        </Link> */}
+        
         <p>Navbar</p>
         <p>Navbar</p>
-        <p>Navbar</p>
-        <p>Navbar</p>
+      </div>
+      <div className="p-8 mr-6 bg-slate-950">
+        <p>User</p>
       </div>
     </nav>
   );
