b5b4264ae12c22134f979cac31bdace40260084a
Renato <renatoft89@gmail.com>
2024-10-03 16:11:55 -0300

    feat: add navbar

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -8,17 +8,23 @@ const Navbar = (() => {
   const router = useRouter();
   const currentPath = router.asPath;
   return (
-    <nav className="container-md min-h-32 bg-sky-500 flex items-center justify-between ">
+    <nav className="container-md min-h-32 bg-sky-700 flex items-center justify-between ">
       <div className="flex px-8">
-        {/* <Link to="/" className={currentPath === '/' ? 'disabled-link' : 'active-link'}>
+        <Link href="/" className={`text-slate-200 pl-4 font-bold ${currentPath === '/' ? 'disabled-link' : 'active-link'}`}>
           Home
-        </Link> */}
-        
-        <p>Navbar</p>
-        <p>Navbar</p>
+        </Link>
+        <Link href="/megasena" className={`text-slate-200  pl-4 font-mono ${router.pathname === '/megasena' ? 'active-link' : 'default-link'}`}>
+            Mega Sena
+          </Link>
+          <Link href="/lotofacil" className={`text-slate-200  pl-4 font-mono ${router.pathname === '/lotofacil' ? 'active-link' : 'default-link'}`}>
+            Lotofácil
+          </Link>
+          <Link href="/quina" className={`text-slate-200 pl-4 font-mono ${router.pathname === '/quina' ? 'active-link' : 'default-link'}`}>
+            Quina
+          </Link>
       </div>
-      <div className="p-8 mr-6 bg-slate-950">
-        <p>User</p>
+      <div className="p-8 mr-6">
+        <p className="text-slate-50 font-mono">Renato Alves</p>
       </div>
     </nav>
   );
