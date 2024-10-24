a9fe1d925bb08cc085202cbb05fe57ff63cd38a5
Renato <renatoft89@gmail.com>
2024-10-24 20:52:09 -0300

    fix: footer text center

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -5,11 +5,10 @@ const Footer = () => {
     <footer className="bg-sky-600 text-white py-6 mt-auto">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center md:flex-row md:justify-between">
-          <div className="mb-4 md:mb-0 text-center md:text-left">
+          <div className="mb-4 md:mb-0 text-center md:text-center md:flex-1">
             <h2 className="text-lg font-bold">Hipersorte</h2>
             <p className="text-sm">© {new Date().getFullYear()} Todos os direitos reservados.</p>
           </div>
-          
         </div>
       </div>
     </footer>
