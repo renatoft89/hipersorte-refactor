33523a34d500ab19c7b4212ede3c09953c5ebe08
renatoft89 <renatoft89@gmail.com>
2024-11-25 14:07:50 -0300

    feat: init Register users

--- front/src/app/RegisterUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const RegisterUser = () => {
-  return (
-    <div>Registrar Usuario</div>
-  )
-}
---
+++ front/src/app/register/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+const RegisterUser = () => {
+  return (
+    <div>Registrar Usuario</div>
+  );
+}
+
+export default RegisterUser;
